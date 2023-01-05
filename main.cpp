9b4e21fedddc0412f8a699a4727eb1b5cfb965a2
abreathingcorpse <raul.rst11@gmail.com>
2023-01-05 06:33:23 -0600

    Created a header file, because the main.cpp was getting too big

--- main.cpp
+++ main.cpp
@@ -1,44 +1,10 @@
-
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "support.h"
 
-// Declaring the SFML namespace
+// Declaring the namespaces
 using namespace sf;
 using namespace std;
-
-// --- Character ---
-// Define the constant parameters
-float TRIANGLE_BASE = 18.0f;
-float TRIANGLE_HEIGHT = 20.0f;
-float TAILCUT_HEIGHT = 4.0f; // Must: TAILCUT_HEIGHT < TRIANGLE_HEIGHT
-
-// Declare a ConvexShape for the character
-ConvexShape character;
-// NOTE: The area before the main() is to declare stuff only,
-// any attempts to run anything outside of it will result in an out of scope issue
-ContextSettings settings;
-// Define the rotation angle
-float angle = 45;
-// --- # ---
-
-// --- RenderWindow ---
-// This is toggled between true/false whenever the player presses F
-bool fullscreen_flag = true;
-// --- # ---
-
-// Receives the referece of window, as well as the fullscreen_flag
-// Toggles between default and Fullscreen mode
-void toggle_fullscreen(RenderWindow& window, bool fullscreen_flag) {
-	if (fullscreen_flag) {
-		VideoMode DesktopVideoMode = VideoMode::getDesktopMode();
-		window.create(DesktopVideoMode, "Small Game", Style::Fullscreen, settings);
-		window.clear(Color::Black);
-		// window.draw(...)
-		window.display();
-	} else {
-		window.create(VideoMode(800,600), "Small Game", Style::Default, settings);
-	}
-}
 
 int main() {
 	
